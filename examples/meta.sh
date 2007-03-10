5125437d3fe913eb1a57f84cde5bdac2fd14c813
moritz <moritz@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2007-03-10 20:13:56 +0000

    Move conf/ to examples/. Add ezstream_metadata.xml and two minimalistic example scripts.
    git-svn-id: https://svn.xiph.org/trunk/ezstream@12710 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ examples/meta.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Example metadata script that has the behavior required by ezstream.
+
+test -z "${1}" && echo "Ezstream presents"
+test x"${1}" = "xartist" && echo "Great Artist"
+test x"${1}" = "xtitle" && echo "Great Song"
---
+++ examples/play.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Example playlist script that has the behavior required by ezstream.
+
+echo "Great_Artist_-_Great_Song.ogg"
