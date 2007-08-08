134ebe32778f47c5100a13723ac294589be8577d
moritz <moritz@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2007-08-08 13:25:12 +0000

    Damn, it looks like I forgot to update this for the Win32 builds ...
    git-svn-id: https://svn.xiph.org/trunk/ezstream@13461 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- win32/config.h
+++ win32/config.h
@@ -8,22 +8,22 @@
 #define PACKAGE "ezstream"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "grmo@users.sourceforge.net"
+#define PACKAGE_BUGREPORT "https://trac.xiph.org/newticket?component=ezstream"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "ezstream"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ezstream 0.4.1"
+#define PACKAGE_STRING "ezstream 0.4.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "ezstream"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.4.1"
+#define PACKAGE_VERSION "0.4.4"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.4.1"
+#define VERSION "0.4.4"
