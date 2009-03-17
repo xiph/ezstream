842d72adc0cd977387ab4b4d668c8ac507f058e1
moritz <moritz@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2009-03-17 18:29:15 +0000

    Bump copyright year where applicable.
    git-svn-id: https://svn.xiph.org/trunk/ezstream@15789 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- src/playlist.c
+++ src/playlist.c
@@ -1,6 +1,6 @@
 /*	$Id$	*/
 /*
- * Copyright (c) 2007 Moritz Grimm <mdgrimm@gmx.net>
+ * Copyright (c) 2007, 2009 Moritz Grimm <mdgrimm@gmx.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
